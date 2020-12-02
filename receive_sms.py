d3cbba790c07a85553169cb67784e5a94e809cf8
nathaniel-prog <66939923+nathaniel-prog@users.noreply.github.com>
2020-12-02 17:01:04 +0200

    the file working fine

--- receive_sms.py
+++ receive_sms.py
@@ -7,17 +7,17 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app= Flask(__name__)
 
-@app.route("/smss", methods=['GET', 'POST'])
+
+
+@app.route("/sms", methods=['GET', 'POST'])
 def send_sms():
     resp=MessagingResponse()
-
-
-    resp.message('il est temps ')
+    resp.message('il est temps !!')
 
 
     return str(resp)
 
-    if __name__ == '__main__':
-        app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
